1560a26aa073a2cec4d9bbdb9417b2d31d5b845a
heicx_sudo <heicx>
2015-09-15 00:37:11 +0800

    directive

---
+++ directive/app.js
@@ -0,0 +1,4 @@
+define(["angular", "angular-route"], function(angular) {
+	console.log(angular);
+	return angular.module("myApp", ["ngRoute"]);
+});
---
+++ directive/main.js
@@ -0,0 +1,15 @@
+require.config({
+	paths: {
+		"angular": "../bower_components/angular/angular.js",
+		"angular-route": "../bower_components/angular-route/angular-route.js"
+	},
+	shim: {
+		"angular": {
+			exports: "angular"
+		},
+		"angular-route": {
+			exports: "angular-route",
+			deps: ["angular"]
+		}
+	}
+})
