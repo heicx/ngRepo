712f93d08971ee6bebdda5a82c3e34c8fa96b126
heicx_sudo <heicx_sudo@163.com>
2015-09-17 20:39:46 +0800

    update

--- directive/app.js
+++ directive/app.js
@@ -1,15 +1,33 @@
 define(["angular"], function(angular) {
 	angular.module("myApp", [])
+	.provider("age", function() {
+		this.age = 18;
+		this.$get = function() {
+			return this.age + "years old."
+		}
+	})
 	.factory('property', function(){
 		return {
 			name: "heicx"
 		};
 	})
-	.controller("MyCtrl", ["$scope", "$injector", function($scope, $injector) {
+	.controller("MyCtrl", ["$scope", "$injector", "property", "age", function($scope, $injector, property, age) {
 		$scope.name = "test";
-
-		$injector.invoke(function(property) {
-			console.log(property.name);
+		$injector.invoke(function(age) {
+			console.log(age);
 		});
-	}]);
+		console.log(age);
+	}]).
+	directive("content", function() {
+		return {
+			restrict: "E",
+			replace: true,
+			template: "<input type='button' value='replace' data-status='1'>",
+			link: function(scope, element, attrs) {
+				element.bind("click", function() {
+					console.log("click");
+				});
+			}
+		}
+	});
 });
