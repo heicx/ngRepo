cd91496d1df8e771f8b466579744c59d77077de0
heicx_sudo <heicx>
2015-09-18 00:20:18 +0800

    http

---
+++ http/index.js
@@ -0,0 +1,35 @@
+angular.module("myApp", []).
+controller("HttpCtrl", ["$scope", "$http", function($scope, $http) {
+	$scope.fetch = function() {
+		alert(123);
+	}
+}]).
+directive('jxbBindCompiledHtml', function ($compile) {
+  'use strict';
+
+  return {
+    template: '<div></div>',
+    scope: {
+      rawHtml: '=jxbBindCompiledHtml'
+    },
+    link: function (scope, elem, attrs) {
+      scope.$watch('rawHtml', function (value) {
+
+        if (!value) {
+          return;
+        }
+
+        // we want to use the scope OUTSIDE of this directive
+        // (which itself is an isolate scope).
+        var newElem = $compile($.parseHTML(value))(scope.$parent);
+        elem.contents().remove();
+        elem.append(newElem);
+      });
+    }
+  };
+});
+
+angular.element(document).ready(function() {
+	//没有设置ng-app，需要手动指定并启动加载module模块。
+	angular.bootstrap(document, ["myApp"]);
+})
