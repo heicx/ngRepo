509237f76b0e5b3b9a9c22f225533fd916dd73b3
heicx_sudo <heicx_sudo@163.com>
2015-09-22 14:14:46 +0800

    http

--- http/index.js
+++ http/index.js
@@ -1,30 +1,32 @@
 angular.module("myApp", []).
-directive('jxbBindCompiledHtml', function($compile) {
-      'use strict';
+controller("HttpCtrl", ["$scope", "$http", function($scope, $http) {
+    $scope.fetch = function() {
+        $http.get("data.json").
+        success(function(data) {
+            $scope.data = data;
+        }).
+        error(function(data) {
+            console.log(data);
+        });
+    }
 
-      return {
-            template: '<div></div>',
-            scope: {
-                  rawHtml: '=jxbBindCompiledHtml'
-            },
-            link: function(scope, elem, attrs) {
-                  scope.$watch('rawHtml', function(value) {
-
-                        if (!value) {
-                              return;
-                        }
-
-                        // we want to use the scope OUTSIDE of this directive
-                        // (which itself is an isolate scope).
-                        var newElem = $compile($.parseHTML(value))(scope.$parent);
-                        elem.contents().remove();
-                        elem.append(newElem);
-                  });
-            }
-      };
+    $scope.data = {
+        name: "jQuery",
+        version: "1.83"
+    }
+}]).
+directive("customer", function() {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            obj: "=sb"
+        },
+        template: "<div><div>name：{{obj.name}}</div><div>version：{{obj.version}}</div><div>"
+    }
 });
 
 angular.element(document).ready(function() {
-      //没有设置ng-app，需要手动指定并启动加载module模块。
-      angular.bootstrap(document, ["myApp"]);
-})
+    //没有设置ng-app，需要手动指定并启动加载module模块。
+    angular.bootstrap(document, ["myApp"]);
+});
